a46274e5ea7425e878ddf70e05b056278a8a75f0
Lavosierdq <lavosierbarbosa@gmail.com>
2023-08-01 20:56:56 -0300

    exercicio da aula 3 finalizado

---
+++ aula3/exercicios/exercicio1.sql
@@ -0,0 +1,49 @@
+create  database cursoSql
+use database cursoSql
+
+create table tipo(
+  Codigo int unsigned auto_increment not null,
+  tipo varchar(40) not null,
+  primary key(codigo)
+);
+
+create  table instrutor(
+  codigo int unsigned not null auto_increment,
+  instrutor varchar (40) not null,
+  telefone  varchar (10) not null,
+  primary key (codigo)
+);
+
+create table curso(
+  codigo int unsigned not null auto_increment,
+  tipo   int unsigned,
+  instrutor int unsigned,
+  curso varchar(40) not null,
+  valor double unsigned not null,
+  primary key (codigo),
+  constraint tipo_fk  foreign key (tipo) references tipo (codigo),
+  constraint instrutor_fk foreign key(instrutor) references instrutor(codigo)
+);
+create table aluno(
+  codigo int unsigned not null auto_increment,
+  aluno varchar (40) not null,
+  endereco varchar(40) not null,
+  email varchar(40) not null,
+  primary key (codigo)
+);
+
+alter table aluno add column data_nascimento varchar (10);
+alter table aluno change column data_nascimento nascimento date
+alter table instrutor add column email varchar(100);
+
+-- mudar o nome da colula
+alter table instrutor change column instrutor nome_instrutor varchar(40) not null;
+alter table Curso change column curso nome_curso varchar(40) not null;
+-- remover o campo de uma tabela
+alter table instrutor drop column email;
+-- criar indice
+create index index_codigo on instrutor(codigo);
+create index index_codigo on curso(curso);
+create index index_aluno on aluno(aluno);
+create index index_curso on curso(instrutor);
+
