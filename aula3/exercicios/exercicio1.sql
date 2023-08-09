908164a21fea0151b9de16e332579b7a59b06265
Lavosierdq <lavosierbarbosa@gmail.com>
2023-08-08 23:23:21 -0300

    corrigindo erros do exercicio da aula 3

--- aula3/exercicios/exercicio1.sql
+++ aula3/exercicios/exercicio1.sql
@@ -31,6 +31,22 @@ create table aluno(
   email varchar(40) not null,
   primary key (codigo)
 );
+create table pedido (
+  codigo int unsigned not null auto_increment,
+  aluno  int unsigned note null auto_increment,
+  data date,
+  horario time,
+  PRIMARY KEY(codigo),
+  constraint aluno_fk FOREIGN KEY(aluno) REFERENCES aluno(codigo)
+);
+
+create table pedido_detalhe(
+  codigo int unsigned not null auto_increment,
+  curso int unsigned not null,
+  valor double unsigned not null
+  PRIMARY KEY(codigo),
+  Constraint curso_fk FOREIGN KEY(curso) REFERENCES curso(codigo)
+);
 
 alter table aluno add column data_nascimento varchar (10);
 alter table aluno change column data_nascimento nascimento date
@@ -46,4 +62,3 @@ create index index_codigo on instrutor(codigo);
 create index index_codigo on curso(curso);
 create index index_aluno on aluno(aluno);
 create index index_curso on curso(instrutor);
-
