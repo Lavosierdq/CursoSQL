4e7be08b5a83385ef9c2410310a9e9b0c90c3868
Lavosierdq <lavosierbarbosa@gmail.com>
2023-08-08 23:21:46 -0300

    concluíndo exercicios aula 4.

--- aula4/exercicios/exercicio1.sql
+++ aula4/exercicios/exercicio1.sql
@@ -14,9 +14,34 @@ insert into curso(codigo,tipo,instrutor,curso,valor) values (2,2,2,'Java Avança
 insert into curso(codigo,tipo,instrutor,curso,valor) values (3,1,1,'SQL Completo',170);
 insert into curso(codigo,tipo,instrutor,curso,valor) values (4,'PHP Basico',2,1,270);
 
+-- inserindo valores na tablea aluno
 insert into aluno(codigo,nome,endereco,email) values (1,'José','Rua XV de novembro','jose@softblue.com.br');
 insert into aluno(codigo,nome,endereco,email) values (2,'Wagner','Av.Paulista','Wagner@softblue.com.br');
 insert into aluno(codigo,nome,endereco,email) values (3,'Emilio','Rua Lajes 103','emilio@softblue.com.br');
 insert into aluno(codigo,nome,endereco,email) values (4,'Cris','Rua Taunei 22','cris@softblue,com.br');
 insert into aluno(codigo,nome,endereco,email) values (6,'Fernando','Av.Central 30','fernando@softblue.com.br');
+
+-- inserindo valores na tabela pedido
+insert into pedido(codigo,aluno,data,horario) values (1,2,'15-04-2010','11:23:32');
+insert into pedido(codigo,aluno,data,horario) values (2,2,'15-04-2010','14:36:21');
+insert into pedido(codigo,aluno,data,horario) values (3,3,'16-04-2010','11:17:45');
+insert into pedido(codigo,aluno,data,horario) values (4,4,'17-04-2010','11:27:22');
+insert into pedido(codigo,aluno,data,horario) values (5,5,'18-04-2010','11:18:19');
+insert into pedido(codigo,aluno,data,horario) values (6,6,'19-04-2010','13:47:35');
+insert into pedido(codigo,aluno,data,horario) values (7,6,'20-04-2010','18:13:44');
+
+--inserindo valores na tabela pedido_detalhe
+insert into pedido_detalhe(codigo,curso,valor) values (1,1,270);
+insert into pedido_detalhe(codigo,curso,valor) values (1,2,330);
+insert into pedido_detalhe(codigo,curso,valor) values (2,1,270);
+insert into pedido_detalhe(codigo,curso,valor) values (2,2,330);
+insert into pedido_detalhe(codigo,curso,valor) values (2,3,170);
+insert into pedido_detalhe(codigo,curso,valor) values (3,4,270);
+insert into pedido_detalhe(codigo,curso,valor) values (4,2,330);
+insert into pedido_detalhe(codigo,curso,valor) values (4,4,270);
+insert into pedido_detalhe(codigo,curso,valor) values (5,3,170);
+insert into pedido_detalhe(codigo,curso,valor) values (6,3,170);
+insert into pedido_detalhe(codigo,curso,valor) values (7,4,270);
+
+
 
