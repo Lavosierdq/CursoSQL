2492605176bc15569bb0b7f7bfdf1e7da3ca2c65
Lavosier Barbosa <lavosierbarbosa@gmail.com>
2023-09-13 23:27:12 -0300

    fazendo exercicios 2 da aula de 6 de SQL

---
+++ aula6/exercicio2.sql
@@ -0,0 +1,60 @@
+/* 1:Utilizando subquery, exiba uma lista com os nomes dos
+cursos disponibilizados pela Softblue informando para cada 
+curso qual o seu menor valor de venda já praticado;
+*/
+
+select curso.curso from curso where curso in (
+  select curso from curso group by valor having min(valor)
+);
+
+-- Resposta
+select curso, (select min(valor) from pedido_detalhe 
+  where pedido_detalhe.curso = curso.codigo) as menor_valor from curso;
+/*
+  Ressalvas para a resposta..
+  select curso -> seleciona a tabela curso
+  select min(valor) from pedido_detalhe; -> seleciona o atributo valor da tabela pedido_detalhe.
+  wherer pedido_detalhe.curso = curso.codigo -> seleciona o atributo de acordo com a condição.
+  as menor_valor -> criou uma alias  do atributo valor renomeado como menor_valor -> criou uma alias  do atributo valor renomeado como menor_valor.
+  from curso -> vindo da tabela curso.
+*/
+-- -------------------------------------------------------------------------------------
+/*
+  2:Utilizando subquery e o parâmetro IN, exiba os nomes dos cursos 
+  disponibilizados pela Softblue cujo tipo de curso seja 'Programação';
+*/
+
+select curso from curso where curso in (
+  select tipo from curso where curto.tipo  = 2 order by
+);
+
+-- Resposta
+select curso from curso where tipo in(
+  select codigo from tipo where tipo = 'programacao'
+)
+
+select curso,tipo.tipo from curso where tipo in (
+  select codigo from tipo where tipo = 'programaçao'
+);
+---------------------------------------------------------------------------------------------
+/*
+  3:Utilizando subquery e o parâmetro EXISTS, exiba novamente os nomes dos
+  cursos disponibilizados pela Softblue cujo tipo de curso seja 'Programação';
+*/
+
+select curso from curso where  exists(
+  select codigo from tipo where tipo  = 'programaçao'
+);
+---------------------------------------------------------------------------------------------
+/*
+  4:Exiba uma lista com os nomes dos instrutores da Softblue e ao lado 
+  o total acumulado das vendas referente aos cursos pelo qual o instrutor 
+  é responsável;
+*/
+
+select instrutor from instrutor where  instrutor in(
+  select curso from pedido_detalhe where instrutor.codigo = pedido_detalhe.pedido
+  group by instrutor having sum(valor);
+)
+
+
