384e10fd5837e58f620601c03b7140ecaef2a285
Lavosier Barbosa <lavosierbarbosa@gmail.com>
2023-09-14 23:21:06 -0300

    Fazendo exercicios da aula 6 sobre sub Querys

--- aula6/exercicio2.sql
+++ aula6/exercicio2.sql
@@ -10,6 +10,10 @@ select curso.curso from curso where curso in (
 -- Resposta
 select curso, (select min(valor) from pedido_detalhe 
   where pedido_detalhe.curso = curso.codigo) as menor_valor from curso;
+
+select curso,(select min(valor)from pedido_detalhe
+where pedido_detalhe.curso = curso.codigo) as menor_valor from curso
+
 /*
   Ressalvas para a resposta..
   select curso -> seleciona a tabela curso
@@ -36,6 +40,8 @@ select curso from curso where tipo in(
 select curso,tipo.tipo from curso where tipo in (
   select codigo from tipo where tipo = 'programaçao'
 );
+
+
 ---------------------------------------------------------------------------------------------
 /*
   3:Utilizando subquery e o parâmetro EXISTS, exiba novamente os nomes dos
@@ -56,5 +62,30 @@ select instrutor from instrutor where  instrutor in(
   select curso from pedido_detalhe where instrutor.codigo = pedido_detalhe.pedido
   group by instrutor having sum(valor);
 )
+
+-- Resposta
+select instrutor,(select sum(pedido_detalhe.valor) from pedido_detalhe 
+  inner join curso on pedido_detalhe.curso = curso.codigo
+  where curso.instrutor = instrutor.codigo)
+  as total_de_vendas from instrutor;
+
+  -------------------------------------------------------------------------------------------
+/*
+  5:Crie uma visão que exiba os nomes dos alunos e quanto cada um já 
+  comprou em cursos;
+*/
+
+create view select nome (
+  select avg(valor) from pedido_detalhe
+  inner join pedido on pedido.aluno = pedido_detalhe.curso
+  order by valor
+)
+from curso;
+-- Resposta
+CREATE VIEW ALUNOS_E_COMPRAS AS SELECT ALUNO,
+  (SELECT SUM(PEDIDO_DETALHE.VALOR) FROM PEDIDO_DETALHE 
+    INNER JOIN PEDIDO ON PEDIDO_DETALHE.PEDIDO = PEDIDO.CODIGO 
+    WHERE PEDIDO.ALUNO = ALUNO.CODIGO) 
+AS TOTAL_DE_COMPRAS FROM ALUNO;
 
 
